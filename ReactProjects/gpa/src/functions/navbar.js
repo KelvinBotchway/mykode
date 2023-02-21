b8256e230f7d9bc1fe11b7d7c72110e9841d56df
Kelvin Botchway <Kelvinjay1000@gmail.com>
2023-02-21 02:15:15 +0000

    Added files

--- ReactProjects/gpa/src/functions/navbar.js
+++ ReactProjects/gpa/src/functions/navbar.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Navbar() {
-  return <h1>Hello welcome to the new gpa calculator</h1>;
+  return (
+    <>
+    <h1 className="">
+      Helloo welcome to the new gpa calculator
+    </h1>
+    <p className="text-3xl underline">We are here</p>
+    </>
+  );
 }
 
 export default Navbar;
