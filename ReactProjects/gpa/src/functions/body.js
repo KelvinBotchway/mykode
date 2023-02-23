1fc878f96f24ac5982eb3bf2c5f7f354eaad7222
Kelvin Botchway <Kelvinjay1000@gmail.com>
2023-02-23 01:10:03 +0000

    added body

--- ReactProjects/gpa/src/functions/body.js
+++ ReactProjects/gpa/src/functions/body.js
@@ -2,15 +2,15 @@ import React, { Component } from "react";
 
 function Body() {
   return (
-    <div className="bg-slate-900 text-white mx-20 h-96 text-left flex flex-row">
-      <div className="m-10">
+    <div className="bg-slate-900 text-white mx-20 h-96 text-left flex justify-between">
+      <div className="m-10 bg-red-500 w-16">
         <h1>GPA Calculator</h1>
         <p>
           Calculate your University of Ghana GPA, check your grades and
           understand how the Gpa scale works
         </p>
       </div>
-      <div className="px-5">Hello</div>
+      <div className="bg-blue-500 w-16">Hello</div>
     </div>
   );
 }
