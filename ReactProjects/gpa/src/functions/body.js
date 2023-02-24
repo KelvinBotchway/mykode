c14282af933deabae88262c514987892e112edf5
Kelvin Botchway <Kelvinjay1000@gmail.com>
2023-02-24 21:06:29 +0000

    Fixed flex alignment

--- ReactProjects/gpa/src/functions/body.js
+++ ReactProjects/gpa/src/functions/body.js
@@ -2,16 +2,41 @@ import React, { Component } from "react";
 
 function Body() {
   return (
-    <div className="bg-slate-900 text-white mx-20 h-96 text-left flex justify-between">
-      <div className="m-10 bg-red-500 w-16">
-        <h1>GPA Calculator</h1>
-        <p>
-          Calculate your University of Ghana GPA, check your grades and
-          understand how the Gpa scale works
-        </p>
+    <>
+      <div className="bg-slate-900 text-white mx-20 h-96 text-left flex justify-between  ">
+        <div className=" bg-slate-900 p-5 mx-5 ">
+          <h1 className="pt-5">GPA Calculator</h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+          nesciunt nihil vitae totam ipsa earum repudiandae optio molestiae eius
+          facilis voluptate adipisci harum illum, minima corporis quibusdam
+          molestias animi placeat!
+        </div>
+        <div className="bg-slate-900 p-5 mx-5">
+          <h1 className="pt-5">GPA Calculator</h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. In eveniet
+          quo minus aut magni, quos magnam vitae tempore laborum neque, impedit
+          laudantium natus accusamus rem totam nulla? Repellendus, magnam
+          voluptate.
+        </div>
       </div>
-      <div className="bg-blue-500 w-16">Hello</div>
-    </div>
+    </>
+    // <>
+    //   <div className="bg-slate-900 text-white mx-20 h-96 text-left flex justify-between">
+    //     <div className="bg-red-500 px-5">
+    //       <h1>GPA Calculator</h1>
+    //       Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+    //       nesciunt nihil vitae totam ipsa earum repudiandae optio molestiae eius
+    //       facilis voluptate adipisci harum illum, minima corporis quibusdam
+    //       molestias animi placeat!
+    //     </div>
+    //     <div class="bg-blue-500 px-5">
+    //       Lorem ipsum dolor sit amet consectetur adipisicing elit. In eveniet
+    //       quo minus aut magni, quos magnam vitae tempore laborum neque, impedit
+    //       laudantium natus accusamus rem totam nulla? Repellendus, magnam
+    //       voluptate.
+    //     </div>
+    //   </div>
+    // </>
   );
 }
 
