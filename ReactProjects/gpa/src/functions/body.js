36d7b1c497969aca4c73bb51ad4a2fdeeb35610c
Kelvin Botchway <Kelvinjay1000@gmail.com>
2023-03-11 14:37:26 +0000

    Added semester div

--- ReactProjects/gpa/src/functions/body.js
+++ ReactProjects/gpa/src/functions/body.js
@@ -6,37 +6,21 @@ function Body() {
       <div className="bg-slate-900 text-white mx-20 h-96 text-left flex justify-between  ">
         <div className=" bg-slate-900 p-5 mx-5 ">
           <h1 className="pt-5">GPA Calculator</h1>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
-          nesciunt nihil vitae totam ipsa earum repudiandae optio molestiae eius
-          facilis voluptate adipisci harum illum, minima corporis quibusdam
-          molestias animi placeat!
+          Calculate your high school, college and cumulative GPA, check your
+          grades and understand how the GPA scale works.
         </div>
-        <div className="bg-slate-900 p-5 mx-5">
-          <h1 className="pt-5">GPA Calculator</h1>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. In eveniet
-          quo minus aut magni, quos magnam vitae tempore laborum neque, impedit
-          laudantium natus accusamus rem totam nulla? Repellendus, magnam
-          voluptate.
-        </div>
+        <div className="bg-slate-900 p-5 mx-5"></div>
+      </div>
+      <div>
+        <ul>
+          <li>
+            <div>
+              <h3>Semester</h3>
+            </div>
+          </li>
+        </ul>
       </div>
     </>
-    // <>
-    //   <div className="bg-slate-900 text-white mx-20 h-96 text-left flex justify-between">
-    //     <div className="bg-red-500 px-5">
-    //       <h1>GPA Calculator</h1>
-    //       Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
-    //       nesciunt nihil vitae totam ipsa earum repudiandae optio molestiae eius
-    //       facilis voluptate adipisci harum illum, minima corporis quibusdam
-    //       molestias animi placeat!
-    //     </div>
-    //     <div class="bg-blue-500 px-5">
-    //       Lorem ipsum dolor sit amet consectetur adipisicing elit. In eveniet
-    //       quo minus aut magni, quos magnam vitae tempore laborum neque, impedit
-    //       laudantium natus accusamus rem totam nulla? Repellendus, magnam
-    //       voluptate.
-    //     </div>
-    //   </div>
-    // </>
   );
 }
 
