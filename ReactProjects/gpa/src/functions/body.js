257df85ac476688a325c98a52878c9018ba263e4
Kelvin Botchway <Kelvinjay1000@gmail.com>
2023-03-13 01:24:23 +0000

    Made width 50%

--- ReactProjects/gpa/src/functions/body.js
+++ ReactProjects/gpa/src/functions/body.js
@@ -4,22 +4,13 @@ function Body() {
   return (
     <>
       <div className="bg-slate-900 text-white mx-20 h-96 text-left flex justify-between  ">
-        <div className=" bg-slate-900 p-5 mx-5 ">
+        <div className=" bg-slate-900 p-5 mx-5 w-2/4">
           <h1 className="pt-5">GPA Calculator</h1>
-          Calculate your high school, college and cumulative GPA, check your
-          grades and understand how the GPA scale works.
+          <p className="w-2/4">
+            Calculate your high school, college and cumulative GPA,check your
+            grades and understand how the GPA scale works.
+          </p>
         </div>
-        <div className="bg-slate-900 p-5 mx-5"></div>
-      </div>
-      <div>
-        <ul>
-          <li>
-            <div>
-              <h3>Semester</h3>
-            
-            </div>
-          </li>
-        </ul>
       </div>
     </>
   );
